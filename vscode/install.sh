0503fd01cc250dd83b8bf34441fbdaee97131263
Cleiton Domazak <cleiton.domazak@farfetch.com>
2020-01-27 11:33:55 +0000

    [FEAT] Several upgrades

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,34 +1,17 @@
 #!/bin/sh
-if test "$(which code)"; then
+if command -v code >/dev/null; then
 	if [ "$(uname -s)" = "Darwin" ]; then
 		VSCODE_HOME="$HOME/Library/Application Support/Code"
 	else
 		VSCODE_HOME="$HOME/.config/Code"
 	fi
+	mkdir -p "$VSCODE_HOME/User"
 
 	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
 	ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
 	ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
 
-	# from `code --list-extensions`
-	modules="
-akamud.vscode-theme-onedark
-DavidAnson.vscode-markdownlint
-eamodio.gitlens
-foxundermoon.shell-format
-HookyQR.beautify
-ipedrazas.kubernetes-snippets
-korekontrol.saltstack
-mauve.terraform
-ms-azuretools.vscode-azureterraform
-ms-azuretools.vscode-docker
-ms-ossdata.vscode-postgresql
-ms-python.python
-ms-vscode.azure-account
-ms-vscode.Go
-PKief.material-icon-theme
-timonwong.shellcheck"
-	for module in $modules; do
-		"$module" || true
-	done
+	while read -r module; do
+		code --install-extension "$module" || true
+	done <"$DOTFILES/vscode/extensions.txt"
 fi
